1956570a19295537471627919a84005018d5120e
Gonzalo-Gallart <gonzalogallart@gmail.com>
2020-06-23 12:23:41 -0300

    pantalla error404

--- DXT/src/app.js
+++ DXT/src/app.js
@@ -14,3 +14,15 @@ app.use(express.json());
 app.use('/', indexRouter)
 
 app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"))
+
+app.use((req, res, next) => next(createError(404)));
+
+app.use((err, req, res, next) => {
+
+  res.locals.message = err.message;
+  res.locals.path = req.path;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
