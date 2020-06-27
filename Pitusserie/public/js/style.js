1e018016ae5f14d062bac8ffcc2b4d861d15d3dc
Gonzalo-Gallart <gonzalogallart@gmail.com>
2020-06-26 22:22:36 -0300

    arreglo errores varios

--- Pitusserie/public/js/style.js
+++ Pitusserie/public/js/style.js
@@ -2,7 +2,7 @@ let headerMovil = $('#headerMovil')
 
 window.addEventListener('scroll', function() {
     const scroll = window.scrollY
-    if (scroll > 500) {
+    if (scroll > 400) {
         headerMovil.addClass('visible')
     } else {
         headerMovil.removeClass('visible')
--- Pitusserie/src/routes/index.js
+++ Pitusserie/src/routes/index.js
@@ -4,16 +4,18 @@ const userController = require('../controllers/userController')
   
 router.get('/', userController.index);
 
+router.get('/login', userController.login);
+router.post('/login', function(req, res) {
+    res.redirect('/login')
+});
+
 router.get('/register', userController.register);
 router.post('/register', function(req, res) {
     res.redirect('/register')
 });
 
-router.get('/login', userController.login);
-router.post('/login', function(req, res) {
-    res.redirect('/login')
-});
 router.get('/detailProducts', userController.detail);
+
 router.get('/error', function(req, res) {
     res.render('error')
 })
